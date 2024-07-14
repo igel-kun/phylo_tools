afde82e4acddaba5dd5628794f33a01c04c6f794
Igel <mathias.weller@cnrs.fr>
2024-07-15 01:34:50 +0200

    bla

--- utils/diversity.hpp
+++ utils/diversity.hpp
@@ -7,10 +7,11 @@ namespace PT {
   template<StrictPhylogenyType Net, NodeContainerType Nodes, class GammaFunctor, class InheritanceProbFunctor>
   void compute_gammas(const Net& N, const Nodes& nodes_to_save, GammaFunctor&& gamma, InheritanceProbFunctor&& p){
     for(const auto uv: N.edges_postorder()) {
-      const auto& [u, v] = uv.as_pair();
       float& current_gamma = gamma(uv);
-      float tmp = 1.0f;
+      const NodeDesc v = uv.head();
       const float p_val = (N.is_leaf(v) && !test(nodes_to_save, v)) ? 0.0f : p(uv);
+      
+      float tmp = 1.0f;
       if(!N.is_leaf(v)) {
         for(const auto vw: N.out_edges(v)) {
           const float g_vw = gamma(vw);
